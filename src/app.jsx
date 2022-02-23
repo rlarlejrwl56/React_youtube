3af1222dafff65d0872ea1cd1cc379d1bed9ab6e
kimkideok <rlarlejr3178@naver.com>
2022-02-23 18:03:07 +0900

    show video with title

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+  
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDCTCiBe57khNLFLtdIIn89f7KbLvMYV-0", 
+      requestOptions
+      )
+    .then(response => response.json())
+    .then(result => setVideos(result.items))
+    .catch(error => console.log('error', error));
+  },[]);
+  return  <VideoList videos={videos}/>
+  
+}
+
+export default App;
